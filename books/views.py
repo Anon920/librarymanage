59e0a457e7bb767033908f7322bef367070a0917
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-23 15:33:30 +0300

    Deleted permission class

--- books/views.py
+++ books/views.py
@@ -1,20 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import AllowAny
 
 from books.models import Book
-from books.permissions import IsAdminOrIfAuthenticatedReadOnly
 from books.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-    def get_permissions(self):
-        if self.action == 'list':
-            permission_classes = [AllowAny]
-        else:
-            permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-        return [permission() for permission in permission_classes]
 
