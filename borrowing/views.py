c6108cd59056b24eb0986de676730a5d4eaa6f94
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-25 12:34:59 +0300

    Created Borrowing view set

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingRetrieveSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return BorrowingRetrieveSerializer
+        return BorrowingSerializer
