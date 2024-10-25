7aa4eef91c45b16139fa729f899e2d7ba4da4eae
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-25 13:32:09 +0300

    Added permission classes and made the user see only his borrowings

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingRetrieveSerializer
@@ -6,8 +7,12 @@ from borrowing.serializers import BorrowingSerializer, BorrowingRetrieveSerializ
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return BorrowingRetrieveSerializer
         return BorrowingSerializer
+
+    def get_queryset(self):
+        return Borrowing.objects.filter(user=self.request.user)
