79a0f8963dbe33d944142bff61fd88f65af7efcf
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-25 16:48:38 +0300

    Added validated func for borrowing

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -4,11 +4,17 @@ from books.serializers import BookSerializer
 from borrowing.models import Borrowing
 
 
-class BorrowingSerializer(serializers.ModelSerializer):
+class BorrowingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = "id", "borrow_date", "expected_date_returned", "actual_date_returned", "book", "user"
-        read_only_fields = ("borrow_date", "expected_date_returned", "actual_date_returned", "user")
+        read_only_fields = ("borrow_date", "actual_date_returned", "user")
+
+    def validate(self, data):
+        book = data["book"]
+        if book.inventory <= 0:
+            raise serializers.ValidationError("This book is out of stock.")
+        return data
 
     def create(self, validated_data):
         validated_data["user"] = self.context["request"].user
