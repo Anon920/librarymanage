b53e96a08d77d78fab862dd1103024999cd62057
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-25 12:30:59 +0300

    Updated Borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -5,11 +6,11 @@ from books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
-    expected_date_returned = models.DateField()
-    actual_date_returned = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_date_returned = models.DateField(null=True, blank=True)
+    actual_date_returned = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Borrowing of {self.book.title} by {self.user.username} on {self.borrow_date}"
