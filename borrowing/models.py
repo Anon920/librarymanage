a8b19190bdcf12dfee0818001b272777459e64bb
Dima Drozdenko <anonimchik920@gmail.com>
2024-10-30 12:12:37 +0200

    Edit name of parameters expected_date_returned -> expected_return_date, actual_date_returned -> actual_return_date

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.db import models
 
 from books.models import Book
@@ -7,11 +6,11 @@ from books.models import Book
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
-    expected_date_returned = models.DateField(null=True, blank=True)
-    actual_date_returned = models.DateField(null=True, blank=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"Borrowing of {self.book.title} by {self.user.username} on {self.borrow_date}"
+        return f"Borrowing of {self.book.title} by {self.user.email} on {self.borrow_date}"
 
