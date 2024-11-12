95c09cbcf14bb71ed1c7a86a0081426313f79432
Dima Drozdenko <anonimchik920@gmail.com>
2024-11-12 13:53:11 +0200

    Created tests for Authenticated users

--- books/tests/test_books.py
+++ books/tests/test_books.py
@@ -80,3 +80,73 @@ class UnAuthenticatedBookAPITests(TestCase):
         response = self.client.put(url, payload, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedBookAPITests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(
+            username='testuser',
+            password='testpassword'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_list_books(self) -> None:
+        sample_book()
+        sample_book(title="Test2", cover="HARD")
+        sample_book(title="Test3", inventory=11)
+
+        response = self.client.get(BOOK_URL)
+
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_retrieve_book(self) -> None:
+        book = sample_book()
+
+        url = reverse("library:book-detail", kwargs={"pk": book.id})
+        response = self.client.get(url)
+
+        serializer = BookSerializer(book)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_book_authenticated(self) -> None:
+        payload = {
+            "title": "New Book",
+            "author": "New Author",
+            "cover": "SOFT",
+            "inventory": 10,
+            "daily_fee": Decimal("2.04")
+        }
+
+        response = self.client.post(BOOK_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_book_authenticated(self) -> None:
+        book = sample_book()
+        payload = {
+            "title": "Updated Book",
+            "author": "Updated Author",
+            "cover": "HARD",
+            "inventory": 15,
+            "daily_fee": Decimal("3.04")
+        }
+
+        url = reverse("library:book-detail", kwargs={"pk": book.id})
+        response = self.client.put(url, payload, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_destroy_book_authenticated(self) -> None:
+        book = sample_book()
+
+        url = reverse("library:book-detail", kwargs={"pk": book.id})
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
