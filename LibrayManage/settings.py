c9d302a84157ff8e7a3cb06092c55535bda12a95
Dima Drozdenko <anonimchik920@gmail.com>
2024-11-13 17:37:29 +0200

    Hide Secret Key

--- LibrayManage/settings.py
+++ LibrayManage/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y3xkw0_%4*pbcjtr1_i9hv@#9of@b1a0p9@z3z9&)fxq%(-(!o'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
